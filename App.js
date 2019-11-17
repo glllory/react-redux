0538ad986f78acbcdaf2bc45db35d160e6ec0923
Majd Saleh <mjd.s.m@hotmail.com>
2019-11-17 21:12:23 +0300

    environment variables setup using babel's babel-plugin-transform-inline-environment-variables

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { f, auth, database } from './config/config.js';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text></Text>
+      <Text>Test</Text>
     </View>
   );
 }
